65fac3efe59fe280a6bae33679919b1675ec3098
BurhanRaja <burhanraja02@yahoo.com>
2023-09-09 10:01:10 +0530

    added chat reply

--- apps/web/src/components/app/chat/ChatList.tsx
+++ apps/web/src/components/app/chat/ChatList.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ChatInn from "./ChatInn";
+import ReplyChat from "./ReplyChat";
 
 const ChatList = () => {
   return (
     <>
       <div className="flex flex-col flex-grow h-0 p-4 overflow-auto">
         <ChatInn />
+        <ReplyChat />
       </div>
     </>
   );
--- apps/web/src/components/app/chat/ReplyChat.tsx
+++ apps/web/src/components/app/chat/ReplyChat.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-const ChatInn = () => {
+const ReplyChat = () => {
   return (
     <>
-      <div className="flex w-full my-3 max-w-xl">
+      <div className="flex w-full my-3 mt-7 max-w-xl  chat-reply">
         <div
           className="flex-shrink-0 h-12 w-12 rounded-full"
           style={{
@@ -13,6 +13,10 @@ const ChatInn = () => {
             backgroundPosition: "center center",
           }}
         ></div>
+        <p className="text-xs absolute text-gray-500 top-[-24px] left-16">
+          <span className="text-orange-500 mr-2">BurhanRaja</span>
+          <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
+        </p>
         <div className="ms-3">
           <div className="text-white">
             <p className="flex items-center">
@@ -31,4 +35,4 @@ const ChatInn = () => {
   );
 };
 
-export default ChatInn;
+export default ReplyChat;
