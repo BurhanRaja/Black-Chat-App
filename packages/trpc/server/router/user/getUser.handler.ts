61c6c6c4bed55db33e7a37aa4d3fb31778b34b1a
BurhanuddinRaja <mails4burhan@gmail.com>
2023-09-12 11:39:17 +0530

    get and update user added

--- packages/trpc/server/router/user/getUser.handler.ts
+++ packages/trpc/server/router/user/getUser.handler.ts
@@ -1,2 +1,22 @@
 // Get user
 // Get User from session and return
+import { prisma } from "database/src/client";
+import { TrpcSessionUser } from "../../createContext";
+
+type UserDetailsParams = {
+  ctx: {
+    user: NonNullable<TrpcSessionUser>;
+  };
+};
+
+export const getUserDetails = async ({ ctx }: UserDetailsParams) => {
+  const { user } = ctx;
+
+  const userDetails = await prisma.user.findUnique({
+    where: {
+      uniqueId: user.uniqueId,
+    },
+  });
+
+  return userDetails;
+};
