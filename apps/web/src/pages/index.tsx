79ac9c6edbbc413302659e446ca9b860b9875989
BurhanRaja <burhanraja02@yahoo.com>
2023-09-14 08:58:01 +0530

    added design

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,21 +1,33 @@
 import React from "react";
-import Header from "../components/website/Header";
-import Hero from "../components/website/Hero";
-import Section1 from "../components/website/Section1";
-import Section2 from "../components/website/Section2";
-import Section3 from "../components/website/Section3";
+import LoginForm from "../components/login/LoginForm";
+import { AiOutlineClose } from "react-icons/ai";
+import Link from "next/link";
 
-const Website = () => {
+const Login = () => {
   return (
     <>
-      {/* https://dribbble.com/shots/16442461-Hush-Messaging-App-Landing-Page */}
-      <Header />
-      <Hero />
-      <Section1 />
-      <Section2 />
-      <Section3 />
+      <section
+        className="bg-gray-900"
+        style={{
+          backgroundImage: `linear-gradient(to bottom, rgb(185 185 185 / 52%), rgb(4 36 129 / 73%)), url(/images/signup-in-4.jpg)`,
+          height: "100%",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        <div className="flex justify-end absolute right-10 top-5">
+          <Link href="/">
+            <AiOutlineClose className="text-black text-5xl font-bold" />
+          </Link>
+        </div>
+        <div className="flex items-center justify-center mx-auto md:h-screen">
+          {/* Validation is remaining */}
+          <LoginForm />
+        </div>
+      </section>
     </>
   );
 };
 
-export default Website;
+export default Login;
