dd44cb1276e3bf972c327cbde7d965af1ac23834
BurhanRaja <burhanraja02@yahoo.com>
2023-10-16 09:23:06 +0530

    added server panel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import ServerPanel from "@/components/server/server-panel";
 import Avatar from "@/components/ui/avatar";
 import Tooltip from "@/components/ui/tooltip";
 import Image from "next/image";
@@ -5,25 +6,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      {/* <Avatar
-        image="https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80"
-        altname="Some IMage"
-        radius="large"
-        transition={true}
-      /> */}
-      <Tooltip
-        trigger={
-          <button>
-            <Avatar
-              image="https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80"
-              altname="Some IMage"
-              radius="large"
-              transition={true}
-            />
-          </button>
-        }
-        content="Hello"
-      />
+     <ServerPanel />
     </>
   );
 }
--- src/components/server/server-panel.tsx
+++ src/components/server/server-panel.tsx
@@ -1,9 +1,60 @@
 "use client";
+import Avatar from "@/components/ui/avatar";
+import Tooltip from "@/components/ui/tooltip";
 
 const ServerPanel = () => {
-  return <>
-    
-  </>;
+  return (
+    <>
+      <section className="bg-slate-800 w-[75px] h-[100vh] p-3">
+        <div className="my-3">
+          <Tooltip
+            trigger={
+              <button>
+                <Avatar
+                  image="https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80"
+                  altname="Some IMage"
+                  radius="large"
+                  transition={true}
+                />
+              </button>
+            }
+            content="Hello"
+          />
+        </div>
+        <hr className="border-gray-500" />
+        <div className="my-3">
+          <Tooltip
+            trigger={
+              <button>
+                <Avatar
+                  image="https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80"
+                  altname="Some IMage"
+                  radius="large"
+                  transition={true}
+                />
+              </button>
+            }
+            content="Hello"
+          />
+        </div>
+        <div className="my-3">
+          <Tooltip
+            trigger={
+              <button>
+                <Avatar
+                  image="https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80"
+                  altname="Some IMage"
+                  radius="large"
+                  transition={true}
+                />
+              </button>
+            }
+            content="Hello"
+          />
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default ServerPanel;
